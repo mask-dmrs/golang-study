bfdee6fd13955ebfc6374a2ef5a29a4ea00a1295
masque <masque@aliyun.com>
2017-12-11 20:36:54 +0800

    添加随机数种子

--- ch01/02_package.go
+++ ch01/02_package.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
-
+//这个程序的运行环境是确定性的，因此 rand.Intn 每次都会返回相同的数字。 （为了得到不同的随机数，需要提供一个随机数种子，参阅 rand.Seed。）
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	rand.Seed(time.Now().Unix())
+	fmt.Println("My favorite number is", rand.Intn(10));
 }
