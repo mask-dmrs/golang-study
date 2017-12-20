d5d23bf32168dfdf541549ab231bfb2f314af225
masque <masque@aliyun.com>
2017-12-20 21:08:59 +0800

    add

--- ch02/13_type-conversions.go
+++ ch02/13_type-conversions.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"math"
 )
-
+/**
+ * var i int = 42
+ * var f float64 = float64(i)
+ * var u uint = uint(f)
+ */
+ //https://tour.go-zh.org/basics/13
 func main() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*x + y*y))
