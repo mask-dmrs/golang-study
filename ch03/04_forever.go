f2b6e3c5f1738b253bf56d965d474e8142d8d03f
masque <masque@aliyun.com>
2018-01-18 17:48:52 +0800

    add test

--- ch03/04_forever.go
+++ ch03/04_forever.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
  * 如果省略了循环条件，循环就不会结束，因此可以用更简洁地形式表达死循环。
  */
@@ -7,5 +9,6 @@ package main
 //https://tour.go-zh.org/flowcontrol/4
 func main() {
 	for {
+		fmt.Println("loop print test")
 	}
 }
